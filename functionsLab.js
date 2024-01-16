2159a73301c0e41ab0c7084b2d77f473728ccc02
sophmark <148284392+sophmark@users.noreply.github.com>
2024-01-16 23:25:45 +0000

    completed all tasks.

--- functionsLab.js
+++ functionsLab.js
@@ -1,14 +1,31 @@
 // 1. Write a function that:
 //   - Console.logs 'Hello World'
 
+function greeting() {
+  console.log('Hello World');
+}
+greeting();
+
+
 // 2. Write a function with 1 parameter that:
 //  - Console.logs whatever you want it to say.
 //  - This would be a good time to use string interpolation!
+
+function hi(phrase) {
+  console.log(`hi ${phrase}`);
+}
+hi('brisk');
 
 // 3. Write a function with 1 parameter that:
 //  - Prints a random number between 1 and whatever number is for the provided argument. 
 //  - You might need to search how to generate a random number.
 //  - You might need to search how to round a number up.
+
+function number(num) {
+  console.log(Math.ceil(Math.random() * num));
+}
+
+number(682);
 
 // 4. Write a function with 3 parameters that:
 //  - Takes the product of the first two parameters and raises them to the power of the third parameter.
@@ -16,13 +33,32 @@
 //  - Example: Passing this function 1, 2, 3 should give you back the answer to (1 * 2)^3, which is 8. 
 //  - You may need to search how to raise the power of a number!
 
+function mathematics(num1, num2, num3) {
+  return Math.pow(num1 * num2, num3);
+}
+
+console.log(mathematics(3, 3, 3));
+
 // 5. Write a function with 1 parameter that:
 //  - Converts Celsius to Fahrenheit.
 //  - The formula to convert Celsius to Fahrenhiet: F = (C * 1.8) + 32
 
+function moreMath(num) {
+  return (num * 1.8) + 32;
+}
+console.log(moreMath(32));
+
 // 6. Write a function with 1 parameter that:
 //  - Takes a word and consoles the letters in alphabetical order.
 //  - You'll need to break the word apart, sort the letters, and bring them back together for this!
+
+function abcs(word) {
+  let letters = word.split('');
+  letters.sort();
+  console.log(letters.join(''));
+}
+abcs('whywereyougivingmenativecodebefore');
+
 
 // BONUS FUNCTIONS!
 // 7. Write a function that:
@@ -39,3 +75,4 @@
 // 10. Write a function that:
 //  -  Prints every EVEN number between 1 and 100.
 //  - This requires a loop - you may need to look this up!
+
--- functionsPractice.js
+++ functionsPractice.js
@@ -4,6 +4,13 @@
 //   - Multiplies them together, and 
 //   - Logs them to the console.
 
+function multiplyByTwo(num1, num2) {
+  const result = num1 * num2;
+  
+  console.log('RESULT!', result);
+}
+multiplyByTwo(2, 3);
+
 // 1. DECLARE & NAME THE FUNCTION
 //   - What should I name this function?
 
@@ -25,3 +32,12 @@
 // 5. Write a function that will do the following:
 //   - If the correct password “October” is entered, print the message “Success!” to the console
 //   - Else, print “Try again!” if any other password is entered.
+function month(October) {
+  if (October === "October") {
+    console.log("Success!");
+  } else {
+    console.log("Try again!");
+  }
+}
+month('October');
+month('December');
